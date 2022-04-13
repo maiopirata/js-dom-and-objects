05836da576757cf1e861e4f81ff71c4e4512fa74
maiopirata <maiopirata@pop-os.localdomain>
2022-04-13 11:27:32 +0200

    update

--- index.js
+++ index.js
@@ -33,104 +33,134 @@ const Jane = new Person(
 const listOfCohort = [John, Jane, Ann];
 
 // my object person
-const person = {
-    name: 'John Doe',
-    quote: 'My inspirational quote here...',
-    skills: [
-        'HTML',
-        'CSS',
-        'JavaScript'
-    ],
-    getSkills() {
-        return this.skills
-    }
-}
+// const person = {
+//     name: 'John Doe',
+//     quote: 'My inspirational quote here...',
+//     skills: [
+//         'HTML',
+//         'CSS',
+//         'JavaScript'
+//     ],
+//     getSkills() {
+//         return this.skills
+//     }
+// }
 
 // pages options
-const landingPage = 'profile';
-let page = 'profile';
+const landingPage = 'home';
+let page = landingPage;
 
-const changePage = (newPage) => {
+const changePage = (newPage, person) => {
     // change the page variable
     if (newPage === page) return
     page = newPage;
     // empty the content of my page
     wrapper.innerHTML = ""
     // fill the content of the new page
-    loadPage(newPage)
+    loadPage(page, person)
 }
 
-const loadPage = (page) => {
+const loadHomePage = () => {
+    // how to set a h1 inside my wrapper
+    const title = document.createElement('h1')
+    title.setAttribute('id', 'myTitle')
+    title.setAttribute('class', 'title')
+    
+    title.innerHTML = "This is the Home Page!"
+    wrapper.appendChild(title)
+
+    const goToProfileButton = document.createElement('button')
+    goToProfileButton.setAttribute('class', 'button')
+    goToProfileButton.innerHTML = 'Go to profile page'
+
+    wrapper.appendChild(goToProfileButton)
+
+    goToProfileButton.addEventListener(
+        'click',
+        () => {
+            console.log('clicked')
+            changePage('profile')
+        }
+    )
+
+    // here I would like to map over my list of people
+    // show a column content for each one of them
+    const ulist = document.createElement('div');
+    ulist.setAttribute('class', 'cohort-list');
+    wrapper.appendChild(ulist);
+
+    listOfCohort.forEach((person) => {
+        const listItem = document.createElement('div');
+        listItem.setAttribute('class', `profile-item`)
+        listItem.innerHTML = person.name
+        console.log(listItem)
+        ulist.appendChild(listItem)
+
+        const viewButton = document.createElement('button')
+        viewButton.setAttribute('class', 'button')
+        viewButton.innerHTML = 'view'
+    
+        listItem.appendChild(viewButton)
+    
+        viewButton.addEventListener(
+            'click',
+            () => changePage('profile', person)
+        )
+    })
+}
+
+const loadProfilePage = (person) => {
+    // how to set a h1 inside my wrapper
+    const title = document.createElement('h1')
+    title.setAttribute('id', 'myTitle')
+    title.setAttribute('class', 'title')
+
+    title.innerHTML = person.name
+    wrapper.appendChild(title)
+
+    // let's set a quote
+    const quote = document.createElement('p')
+    quote.setAttribute('class', 'quote')
+    quote.innerHTML = `"${person.quote}"`
+    wrapper.appendChild(quote)
+
+    // let's add the skills
+    const ulist = document.createElement('ul');
+    ulist.setAttribute('class', 'skill-list');
+    wrapper.appendChild(ulist);
+
+    person.skills.forEach((skill, iter) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('class', `list-item skill-${iter}`)
+        listItem.innerHTML = skill
+        ulist.appendChild(listItem)
+    })
+
+    const backToCohortButton = document.createElement('button')
+    backToCohortButton.setAttribute('class', 'button')
+    backToCohortButton.innerHTML = 'Back to cohort'
+
+    wrapper.appendChild(backToCohortButton)
+
+    backToCohortButton.addEventListener(
+        'click',
+        () => changePage('home')
+    )
+}
+
+const loadPage = (page, person) => {
     if (page === 'profile') {
-        // how to set a h1 inside my wrapper
-        const title = document.createElement('h1')
-        title.setAttribute('id', 'myTitle')
-        title.setAttribute('class', 'title')
-        
-        title.innerHTML = person.name
-        wrapper.appendChild(title)
-
-        // let's set a quote
-        const quote = document.createElement('p')
-        quote.setAttribute('class', 'quote')
-        quote.innerHTML = `"${person.quote}"`
-        wrapper.appendChild(quote)
-
-        // let's add the skills
-        const ulist = document.createElement('ul');
-        ulist.setAttribute('class', 'skill-list');
-        wrapper.appendChild(ulist);
-
-        person.skills.forEach((skill, iter) => {
-            const listItem = document.createElement('li');
-            listItem.setAttribute('class', `list-item skill-${iter}`)
-            listItem.innerHTML = skill
-            ulist.appendChild(listItem)
-        })
-
-        const backToCohortButton = document.createElement('button')
-        backToCohortButton.setAttribute('class', 'button')
-        backToCohortButton.innerHTML = 'Back to cohort'
-
-        wrapper.appendChild(backToCohortButton)
-
-        backToCohortButton.addEventListener(
-            'click',
-            () => changePage('home')
-        )
+        loadProfilePage(person)
     } else if (page === 'home') {
-        // how to set a h1 inside my wrapper
-        const title = document.createElement('h1')
-        title.setAttribute('id', 'myTitle')
-        title.setAttribute('class', 'title')
-        
-        title.innerHTML = "This is the Home Page"
-        wrapper.appendChild(title)
-
-        const goToProfileButton = document.createElement('button')
-        goToProfileButton.setAttribute('class', 'button')
-        goToProfileButton.innerHTML = 'Go to profile page'
-
-        wrapper.appendChild(goToProfileButton)
-
-        goToProfileButton.addEventListener(
-            'click',
-            () => changePage('profile')
-        )
-
-        // here I would like to map over my list of people
-        // show a column content for each one of them
-        const ulist = document.createElement('div');
-        ulist.setAttribute('class', 'cohort-list');
-        wrapper.appendChild(ulist);
-
-        listOfCohort.forEach(person => {
-            const listItem = document.createElement('div');
-            listItem.setAttribute('class', `list-item skill-${iter}`)
-            listItem.innerHTML = skill
-            ulist.appendChild(listItem)
-        })
+        loadHomePage()
     }
 }
 
-loadPage(landingPage);
+// first load
+loadPage(landingPage); // home
+
+// what happens when the page loads in the browser
+// 1. the browser looks for the html file
+// 2. html is loading css
+// 3. html is creating a div
+// 4. html is loading js
